ec9d52e309487ed994ddeef963cd9581082699c6
DevilDeveloper <iamdevildeveloper@gmail.com>
2026-02-17 10:41:01 +0530

    Learning about bash scripting

---
+++ bash-scripting/hello-world.sh
@@ -0,0 +1,16 @@
+## print 
+# echo "kishan"
+
+## print data in file ( > will replace the file the content with new one, >> will append )
+# ls >> hello-world.txt 
+
+
+##
+
+# 1️⃣ Print Basic System Info
+# When executed, it should display:
+# Current logged-in user
+# Current date & time
+# Hostname
+# Current working directory
+# system_info.sh
